254e15aa17438f4b7c645ffe5e626eb5941aa0c6
root <root@mail.thezilches.com>
2015-08-25 10:10:20 -0400

    Fixed Bug in ip address comparrison

--- myddclient.sh
+++ myddclient.sh
@@ -1,46 +1,49 @@
 #/bin/bash
-function ipcheck {
-        isdiff=1
-        hostlist=$2
+haschanged() {
+        isdiff=0
+#       echo $hostlist
+        hostlist=$1
+        domain="$2"
         ipaddressnew=`dig +short myip.opendns.com @resolver1.opendns.com`
         for host in ${hostlist[@]}
         do
                 if [ $host == "@" ]; then
-                        ipaddresscurrent=`nslookup $1 | grep Address | grep -v "#" | cut -f2 -d" "`
+                        ipaddresscurrent=`nslookup $domain | grep Address | grep -v "#" | cut -f2 -d" "`
                 else
-                        ipaddresscurrent=`nslookup $host.$1 | grep Address | grep -v "#" | cut -f2 -d" "`
+                        ipaddresscurrent=`nslookup $host.$domain | grep Address | grep -v "#" | cut -f2 -d" "`
                 fi
-                diffcheck=`echo $ipaddresscurrent | grep -c $ipaddressnew`
-                if [ $diffcheck -eq 0 ];then
-                        isdiff=0
+                if [ "$ipaddresscurrent" != "$ipaddressnew" ]; then
+                        isdiff=1
                 fi
+
         done
+        return $isdiff
 }
-function update {
-        echo $2
+update() {
         hostlist=$1
+        domain="$2"
+        password="$3"
         for host in ${hostlist[@]}
         do
-                echo $host
-                response=`curl http://dynamicdns.park-your-domain.com/update?host=$host\&domain=$2\&password=$3`
+                response=`curl http://dynamicdns.park-your-domain.com/update?host=$host\&domain=$domain\&password=$password`
                 echo
-                errorcount=`echo $response | perl -pe "s/.*\<errcount\>(\d+)\<\/ErrCount\>.*/\\$1/gi"`
+                errorcount=`echo "\"$response\"" | perl -pe "s/.*\<ErrCount\>(\d+)\<\/ErrCount\>.*/\\$1/gi"`
                 if [ $errorcount -gt 0 ]; then
                         echo $response >> /var/log/ddclient.err
                 fi
         done
 
 }
-isdiff='1'
+isdiff='0'
 
 ######################
 #Copy and paste this code as many times as you need. Change the values in the hostlist for the a records you wish to update, the domain to your domain, and the password as given out by namecheap
 hostlist=("@" "www" "beta")
 domain="example.com"
 password="########################"
-ipcheck $domain $hostlist
-if [ $isdiff -eq 0 ]; then
-        update $hostlist $domain $password
+haschanged $hostlist "$domain"
+if [ $isdiff -gt 0 ]; then
+        update $hostlist "$domain" "$password"
 fi
 ###############################
 
@@ -48,9 +51,9 @@ fi
 #hostlist=("@" "www")
 #domain="mysite.com"
 #password="####################"
-#ipcheck $domain
-#if [ $isdiff -eq 0 ]; then
-#        update $hostlist $domain $password
+#haschanged $hostlist "$domain"
+#if [ $isdiff -gt 0 ]; then
+#        update $hostlist "$domain" "$password"
 #fi
 ##################
 
@@ -59,7 +62,8 @@ fi
 #hostlist=("@" "www" "blog" "sql" "cloud")
 #domain="yetanothersite.com"
 #password="###################"
-#ipcheck $domain
-#if [ $isdiff -eq 0 ]; then
-#        update $hostlist $domain $password
+#haschanged $hostlist "$domain"
+#if [ $isdiff -gt 0 ]; then
+#        update $hostlist "$domain" "$password"
 #fi
+
